42212605a9a2d7578a122af596c857fc2825753f
Nothing <173809041+nothingfr87@users.noreply.github.com>
2026-06-11 20:04:15 +0300

    Implemented memory allocation for better performance

--- src/main.c
+++ src/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  char *buffer = malloc(8192);
+  size_t buffer_size = 8192;
+  char *buffer = malloc(buffer_size);
   FILE *file_ptr = fopen(argv[1], "r");
   if (file_ptr == NULL) {
     perror("Error Occured When Trying to open a file.\n");
     return 1;
   }
-  while (fgets(buffer, sizeof(buffer), file_ptr) != NULL) {
+  while (fgets(buffer, buffer_size, file_ptr) != NULL) {
     printf("%s", buffer);
   }
   free(buffer);
